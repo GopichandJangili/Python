2e97bcbee1446451e83a0e11dd4aa5ceaf31147a
GopichandJangili <68120526+GopichandJangili@users.noreply.github.com>
2020-08-16 13:41:47 +0530

    Add files via upload

--- dataframe_pandas.py
+++ dataframe_pandas.py
@@ -4,6 +4,14 @@
 import pandas as pd
 import numpy as np
 
+df = pd.DataFrame({'A':[1,2,3],'B':['4','5','6']})
+#iterate over columns
+for i in df:
+    print(i)
+#iterate over rows
+for i in df.iterrows():
+    print(i,list(i))
+
 #NULL example
 l = [1,2,np.nan]
 print(l)
@@ -21,7 +29,7 @@ print(df)
 #to list
 l2 = df[0].tolist()
 l3 = df[0]
-print(l2,type(l3))
+print(l2,type(l3))  # col of a DataFrtame is series
 
 #3. Dataframe from list od lists
 l = [['a',2],['b',4],['c',6]]
@@ -31,8 +39,9 @@ print(df)
 #4. Dictionary to DataFrame
 d1 = {'a':[1,2,3],'b':[4,5,6],'c':[7,8,9]}
 df = pd.DataFrame(d1)
+print(df)
 
-#5. Giving indexex
+#5. adding index
 l1 = [[1,2,3],[4,5,6],[7,8,9]]
 df = pd.DataFrame(l1,index=['a','c','z'])
 
@@ -52,7 +61,7 @@ df = pd.DataFrame(a)
 df['c'] = df['a']+df['b']
 print(df)
 
-#9. Case statement in DataFrame
+#9. Case statement in DataFrame (row wise)
 df = pd.DataFrame([[1,2,3],[3,4,5]],columns=['a','b','c'])
 print(df)
 def func(row):
@@ -66,5 +75,73 @@ def func(row):
 
 df['third'] = df.apply(func, axis=1)
 print(df)
+
+#10. Case statement using lambda 
+dict_1 = {'A':[1,2,3],'B':[4,5,6],'C':[1,1,1]}
+df = pd.DataFrame(dict_1)
+def sub(a,b):
+    return a-b
+    
+df['D'] =df.apply(lambda x: x['A']+x['B'],axis=1)
+df['E'] =df.apply(lambda y: sub(y['A'],y['B']) ,axis=1)
+print(df)
+
+#11 DataFrame Columns
+df = pd.DataFrame({'A':[1,2,3,4],'B':[9,9,9,9]})
+l = df.columns.tolist()
+print(l)
+
+#12. Transpose in DataFrame
+df = pd.DataFrame({'A':[1,2,3,4],'B':[9,9,9,9]})
+print(df)
+print(df.T)
+print(df.transpose())
+
+#13 loc and iloc in dataframe (where clause)
+##loc:
+#loc is label-based, which means that we have to 
+#specify the name of the rows and columns that we 
+#need to filter out.
+##iloc:
+#On the other hand, iloc is integer index-based. So 
+#here, we have to specify rows and columns by their integer index.    
+
+df = pd.DataFrame({'A':[1,2,3,4],'B':[50,60,70,80],
+                   'C':[12,19,16,15]})
+df['A'] or df.A #both are same
+
+#where condition
+df1 = df.loc[df.A > 2] 
+df3 = df.loc[df['A'] > 2] 
+print(df1,df3)
+#multiple
+df2 = df.loc[(df.A>2) & (df.B<75)]
+print(df2)
+#using slicing
+print(df,df.loc[1:3],df[0:3])
+#only selected columns
+print(df.loc[(df.A >2),(['A','C'])])
+#update using loc
+df.loc[(df.A>2),(['A','C'])] = [8,9]
+print(df)
+
+##using iloc
+# select rows with indexes
+df.iloc[[0,2]]
+df.iloc[[0,3]]
+df.iloc[[0,3],[1,2]]
+#range of rows using iloc
+df.iloc[:3]
+df.iloc[:2]
+
+
+
+
+
+
+
+
+
+
 
 
